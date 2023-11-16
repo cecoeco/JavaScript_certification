b0353e227cc11883ae9a9f03e81f294424466165
webofceco <mapleselijah9@gmail.com>
2023-11-15 21:16:44 -0500

    js

---
+++ 01_Basic JavaScript/Accessing Object Properties with Bracket Notation.js
@@ -0,0 +1,11 @@
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Use bracket notation to access the 'an entree' property
+  const drinkValue = testObj["the drink"];    // Use bracket notation to access the 'the drink' property
+  
---
+++ 01_Basic JavaScript/Accessing Object Properties with Dot Notation.js
@@ -0,0 +1,11 @@
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Use dot notation to access the 'hat' property
+  const shirtValue = testObj.shirt;    // Use dot notation to access the 'shirt' property
+  
