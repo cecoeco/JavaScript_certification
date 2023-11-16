be1730b0484cacb89b247def30f97807bbc3f575
webofceco <mapleselijah9@gmail.com>
2023-11-15 21:34:35 -0500

    js

---
+++ 01_Basic JavaScript/Add New Properties to a JavaScript Object.js
@@ -0,0 +1,11 @@
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  // Only change code below this line
+  myDog["bark"] = "woof";
+  
---
+++ 01_Basic JavaScript/Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    // Add the new album below this line
+    {
+      "artist": "The Beatles",
+      "title": "Abbey Road",
+      "release_year": 1969,
+      "formats": [
+        "CD",
+        "LP"
+      ]
+    }
+  ];
+  
