e952631e3eee80f14bf53b5269d7def393779d4a
webofceco <mapleselijah9@gmail.com>
2023-12-04 17:04:51 -0500

    more regex

---
+++ 03_Regular Expressions/Check For Mixed Grouping of Characters.js
@@ -0,0 +1,3 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor)(\s\w\.)?\sRoosevelt/;
+let result = myRegex.test(myString);
---
+++ 03_Regular Expressions/Find Characters with Lazy Matching.js
@@ -0,0 +1,6 @@
+// Only change code below this line
+let myRegex = /<.*?>/; 
+// Only change code above this line
+
+let text = "<h1>Winter is coming</h1>";
+let result = text.match(myRegex);
