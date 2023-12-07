d62c6100ccfa724997ac7a8cf45cbb38c2d156c1
webofceco <mapleselijah9@gmail.com>
2023-12-06 22:44:32 -0500

    07_Object Oriented Programming

---
+++ 07_Object Oriented Programming/Use a Mixin to Add Common Behavior Between Unrelated Objects.js
@@ -0,0 +1,21 @@
+let glideMixin = function(obj) {
+  obj.glide = function() {
+    console.log("Gliding, swoosh!");
+  }
+};
+
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+glideMixin(bird);
+glideMixin(boat);
+
+bird.glide();
+boat.glide();
